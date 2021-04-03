ec52176768e9951642833c3486e15f77ec462292
anasari <anasari@paypal.com>
2021-04-03 09:29:48 +0530

    refactor

--- LC/2021/AI/Starter_Codes/starter_code_Q3.py
+++ LC/2021/AI/Starter_Codes/starter_code_Q3.py
@@ -1,13 +1,12 @@
 from sys import maxsize
 from itertools import permutations
 
+
 # implementation of traveling Salesman Problem
 def travellingSalesmanProblem(graph, s):
-
     # Your code here
     n = len(graph)
-    cache = {}
-    cache[(1, 0)] = 0
+    cache = {(1, 0): 0}
     parent = {}
 
     #  state without j  and ending at one of the  i belonging to s
@@ -21,34 +20,33 @@ def travellingSalesmanProblem(graph, s):
     def getMinPath(s, j):
         if (s, j) in cache:
             return cache[(s, j)]
-        prevStates = getPrevStates(s, j)
-        dist, prev = min(
-            (getMinPath(s_j, i) + graph[i][j], (s_j, i)) for s_j, i in prevStates
+        prev_states = getPrevStates(s, j)
+        min_dist, prev_sate = min(
+            (getMinPath(s_j, i) + graph[i][j], (s_j, i)) for s_j, i in prev_states
         )
-        cache[(s, j)] = dist
-        parent[(s, j)] = prev
+        cache[(s, j)] = min_dist
+        parent[(s, j)] = prev_sate
         return cache[(s, j)]
 
-    endSet = (1 << n) - 1
+    end_set = (1 << n) - 1
     dist, prev = min(
-        (getMinPath(endSet, j) + graph[j][0], (endSet, j)) for j in range(1, n)
+        (getMinPath(end_set, j) + graph[j][0], (end_set, j)) for j in range(1, n)
     )
-    parent[(endSet, 0)] = prev
+    parent[(end_set, 0)] = prev
 
-    min_path = [(endSet, 0)]
+    min_path = [(end_set, 0)]
     while min_path[-1] != (1, 0):
         min_path.append(parent[min_path[-1]])
 
-    return [i for s, i in min_path]  # Minimum cost path covering all 7 wonders
+    return [i for _, i in min_path]  # Minimum cost path covering all 7 wonders
 
 
 # Driver Code
 if __name__ == "__main__":
-
     # matrix representation of graph
     graph = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]]
-    locations = ["Great Wall","Colosseum", "Chichen Itza", "Machu Picchu", "Christ", "Petra", "Taj Mahal"]
-    edges =[
+    locations = ["Great Wall", "Colosseum", "Chichen Itza", "Machu Picchu", "Christ", "Petra", "Taj Mahal"]
+    edges = [
         ["Chichen Itza", "Colosseum"]
     ]
     s = 0
